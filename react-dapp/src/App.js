a19dd1100276ffb027d22ed12d84891426ce7c31
Nedim Begić <nedim@renesalis-packaging.com>
2021-05-01 16:21:35 +0200

    Update App.js

--- react-dapp/src/App.js
+++ react-dapp/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import { ethers } from 'ethers'
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 
-const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+// Update with the contract address logged out to the CLI when it was deployed 
+const greeterAddress = "your-contract-address"
 
 function App() {
   // store greeting in local state
